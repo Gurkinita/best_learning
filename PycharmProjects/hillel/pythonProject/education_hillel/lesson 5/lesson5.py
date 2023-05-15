36649676e1b467a2fc949c5a39cf0353975de08a
julia.rodenkova <gurkinita@gmail.com>
2023-05-15 23:13:19 +0300

    убрала комментирование с первого задания))))

--- PycharmProjects/hillel/pythonProject/education_hillel/lesson 5/lesson5.py
+++ PycharmProjects/hillel/pythonProject/education_hillel/lesson 5/lesson5.py
@@ -1,25 +1,25 @@
-# class DiscriminantError(Exception):
-#     pass
-#
-# a = float(input("Enter a: "))
-# b = float(input("Enter b: "))
-# c = float(input("Enter c: "))
-#
-# D = b**2 - 4*a*c
-#
-# try:
-#     if D < 0:
-#         raise DiscriminantError("Дискриминант отрицательный")
-#
-#     sqrt_D = D**0.5
-#     x1 = (-b + sqrt_D) / (2*a)
-#     print("x1 =", x1)
-#
-# except DiscriminantError as e:
-#     print("Ошибка:", str(e))
-#
-#
-#
+class DiscriminantError(Exception):
+    pass
+
+a = float(input("Enter a: "))
+b = float(input("Enter b: "))
+c = float(input("Enter c: "))
+
+D = b**2 - 4*a*c
+
+try:
+    if D < 0:
+        raise DiscriminantError("Дискриминант отрицательный")
+
+    sqrt_D = D**0.5
+    x1 = (-b + sqrt_D) / (2*a)
+    print("x1 =", x1)
+
+except DiscriminantError as e:
+    print("Ошибка:", str(e))
+
+
+
 
 formula = input("Enter your request, for example 1 + 1 (use space): ")
 
