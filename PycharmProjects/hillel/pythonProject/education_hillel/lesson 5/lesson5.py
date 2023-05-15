c680bc125c358c94895320363632036d7224695c
julia.rodenkova <gurkinita@gmail.com>
2023-05-15 23:11:27 +0300

    задание 2

--- PycharmProjects/hillel/pythonProject/education_hillel/lesson 5/lesson5.py
+++ PycharmProjects/hillel/pythonProject/education_hillel/lesson 5/lesson5.py
@@ -1,19 +1,48 @@
-class DiscriminantError(Exception):
-    pass
+# class DiscriminantError(Exception):
+#     pass
+#
+# a = float(input("Enter a: "))
+# b = float(input("Enter b: "))
+# c = float(input("Enter c: "))
+#
+# D = b**2 - 4*a*c
+#
+# try:
+#     if D < 0:
+#         raise DiscriminantError("Дискриминант отрицательный")
+#
+#     sqrt_D = D**0.5
+#     x1 = (-b + sqrt_D) / (2*a)
+#     print("x1 =", x1)
+#
+# except DiscriminantError as e:
+#     print("Ошибка:", str(e))
+#
+#
+#
 
-a = float(input("Enter a: "))
-b = float(input("Enter b: "))
-c = float(input("Enter c: "))
+formula = input("Enter your request, for example 1 + 1 (use space): ")
 
-D = b**2 - 4*a*c
+symbols = formula.split()
 
 try:
-    if D < 0:
-        raise DiscriminantError("Дискриминант отрицательный")
+    if len(symbols) > 3:
+        raise ValueError("Try again. Use exactly 2 numbers and one action.")
 
-    sqrt_D = D**0.5
-    x1 = (-b + sqrt_D) / (2*a)
-    print("x1 =", x1)
+    first_number = float(symbols[0])
+    action = symbols[1]
+    second_number = float(symbols[2])
 
-except DiscriminantError as e:
-    print("Ошибка:", str(e))
+    if action == "+":
+        result = first_number + second_number
+        print("Result:", result)
+    elif action == "-":
+        result = first_number - second_number
+        print("Result:", result)
+    else:
+        raise ValueError("Use another action (+ or -).")
+
+except ValueError as e:
+    print("Error:", str(e))
+except IndexError:
+    print("Use space between numbers and action.")
